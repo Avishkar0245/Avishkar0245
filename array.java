c88b9c98313b4afe7c7ac9bbfe7547a4a5fc98ab
Avishkar0245 <moreavishkar22@gmail.com>
2024-09-25 14:52:19 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,16 @@
+import java.lang.*;
+
+public class Array 
+{
+    {
+        public static void main(string []args)
+
+            int[] numbers = {10,20,30,40,50};
+            System.out.println("\n Array Lenght : " + numbers.Lenght+"\n");
+            for(int i = 0; i< numbers.Lenght; i++)
+            {
+                System.out.println("\t Element at index "+ i+ ":"+ numbers[i]);
+ 
+            }
+   }
+}
